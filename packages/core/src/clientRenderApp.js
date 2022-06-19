25e129210db14a80b4f3d5f83e4feb27fc25a7e9
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-06-20 00:33:53 +0200

    implement app client render

---
+++ packages/core/src/clientRenderApp.js
@@ -0,0 +1,44 @@
+/* eslint-disable import/no-import-module-exports */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { primer } from '@papillonads/components'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { checkAccessibilityIssues } from '@papillonads/library/a11y'
+import { startPagePath } from './route/path'
+import { appRootRoute, listShowPageRoute, infoShowPageRoute, notFoundPageRoute } from './route'
+import { clientAppStore } from './store/client'
+
+/* istanbul ignore next */
+export function App() {
+  const { ErrorBoundary } = primer
+
+  return (
+    <ErrorBoundary>
+      <Provider store={clientAppStore}>
+        <BrowserRouter>
+          <Routes>
+            <Route path={appRootRoute.path} element={<Navigate to={startPagePath} />} />
+            <Route path={listShowPageRoute.path} element={listShowPageRoute.clientComponent()} />
+            <Route path={infoShowPageRoute.path} element={infoShowPageRoute.clientComponent()} />
+            <Route path="*" element={notFoundPageRoute.clientComponent()} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </ErrorBoundary>
+  )
+}
+
+/* istanbul ignore next */
+function renderApp() {
+  checkAccessibilityIssues(React, ReactDOM, 1000)
+
+  const root = ReactDOM.createRoot(document.getElementById('app'))
+  root.render(<App />)
+
+  if (module.hot) {
+    module.hot.accept()
+  }
+}
+
+renderApp()
